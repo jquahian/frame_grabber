474d67faf18f00ce62259b14282fff9125dfcb0c
Joshua Qua Hiansen <jquahian@gmail.com>
2019-02-13 17:42:03 -0500

    fixed cropping problem oops

--- frame_grabber.py
+++ frame_grabber.py
@@ -6,8 +6,6 @@ dirpath = 'D:/auto_focus/subc4'
 
 outpath = 'D:/auto_focus/subc4/frames'
 
-os.mkdir(outpath)
-
 # return list of videos in the directory
 videos = os.listdir(dirpath)
 
@@ -22,8 +20,10 @@ for i in range(0, len(videos)):
 	# start the video capture
 	cap = cv2.VideoCapture(file_path)
 
-	height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
-	width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+	height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+	width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+
+	print(f"video width: {width} \nvideo height: {height}")
 
 	while (cap.isOpened()):
 
@@ -34,7 +34,7 @@ for i in range(0, len(videos)):
 	        print('Read %d frame: ' % count, ret)
 	       	file_name = videos[i]
 	       	frame = cv2.cvtColor(frame.astype(np.uint8), cv2.COLOR_RGB2GRAY)
-	       	frame = frame[60:height+1, width+1]
+	       	frame = frame[:height, :width]
 	        cv2.imwrite(os.path.join(outpath, '{}_frame{:d}.jpg'.format(file_name[:-4], count)), frame)
 	        count += 1
 	    else:
